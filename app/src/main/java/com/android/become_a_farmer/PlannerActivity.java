a3b055b207581ca30f97233b4ddda78cbad2fe85
Kim Yo Han <rladygks329@gmail.com>
2022-09-29 00:37:56 +0900

    refactor: planner  fragment ->  planner activirty
    1. planner fragment를 activity로 변경하였습니다.

--- app/src/main/java/com/android/become_a_farmer/PlannerActivity.java
+++ app/src/main/java/com/android/become_a_farmer/PlannerActivity.java
@@ -1,28 +1,33 @@
 package com.android.become_a_farmer;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.android.become_a_farmer.databinding.ActivityPlannerBinding;
-public class PlannerFragment extends Fragment {
+
+public class PlannerActivity extends AppCompatActivity {
     private ActivityPlannerBinding binding;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = ActivityPlannerBinding.inflate(inflater,container,false);
-        return binding.getRoot();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPlannerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.plannerPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void changeFragment(Fragment frag){
-        getChildFragmentManager()
+        getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.planner_frame, frag)
                 .commit();

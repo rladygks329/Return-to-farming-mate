393be3cc34ece9a38e252d868b74959d5327f943
Kim Yo Han <rladygks329@gmail.com>
2022-10-25 21:59:04 +0900

    feat: SelectCropFragment
    make button dynamically

--- app/src/main/java/com/android/become_a_farmer/SelectCropFragment.java
+++ app/src/main/java/com/android/become_a_farmer/SelectCropFragment.java
@@ -16,7 +16,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.become_a_farmer.databinding.FragmentSelectCropBinding;
-import com.google.android.material.button.MaterialButton;
+import com.android.become_a_farmer.databinding.ViewPlannerBtnBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class SelectCropFragment extends Fragment {
     private FragmentSelectCropBinding binding;
@@ -38,14 +46,7 @@ public class SelectCropFragment extends Fragment {
 
         spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.lightGreen)), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         binding.selectCropTitle.setText(spannableString);
-
-        String[] crops = {"마늘", "쌀", "사과", "양파"};
-        for(int i = 0; i<crops.length; i++) {
-            MaterialButton btn = (MaterialButton) binding.selectCropGrid.getChildAt(i);
-            btn.setText(crops[i]);
-            btn.setTag(crops[i]);
-            btn.setOnClickListener(this::selectView);
-        }
+        getCropsFromDB();
         binding.selectCropNextBtn.setOnClickListener(view1 -> {
             if(selectedView == null){
                 Toast.makeText(getActivity(), "작물을 선택해주세요.", Toast.LENGTH_SHORT).show();
@@ -75,5 +76,57 @@ public class SelectCropFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void getCropsFromDB(){
+        SharedPreferences pref = requireActivity().getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        String regionName = pref.getString("selectedRegion", "");
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("regions")
+                .whereEqualTo("name", regionName)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            return;
+                        }
+                        QuerySnapshot d = task.getResult();
+                        List<String> list = Arrays.asList(d.getDocuments().get(0).get("crop").toString().split(","));
+                        initButtons(list);
+                    }
+                });
+    }
+
+    private void initButtons(List<String> crops){
+        Iterator<String> it = crops.iterator();
+        while(it.hasNext()){
+            ViewPlannerBtnBinding v = ViewPlannerBtnBinding.inflate(getLayoutInflater());
+            String crop = it.next();
+            v.viewPlannerBtnLeft.setText(crop);
+            v.viewPlannerBtnLeft.setTag(crop);
+            v.viewPlannerBtnLeft.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectView(view);
+                }
+            });
+            if(it.hasNext()){
+                crop = it.next();
+                v.viewPlannerBtnRight.setText(crop);
+                v.viewPlannerBtnRight.setTag(crop);
+                v.viewPlannerBtnRight.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        selectView(view);
+                    }
+                });
+            }else{
+                v.viewPlannerBtnRight.setVisibility(View.INVISIBLE);
+            }
+            binding.selectCropLr.addView(v.getRoot());
+            v = null;
+        }
     }
 }
